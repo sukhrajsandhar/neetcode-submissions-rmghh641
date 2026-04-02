1e7116560b996b0200c9fb0416342f96e8d78971
Sukhraj <139310336+sukhrajsandhar@users.noreply.github.com>
2026-04-02 15:09:11 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        reverse = ""
+        reverse += s
+        reverse = reverse[::-1]
+        reverse = reverse.lower()
+        reverse = re.sub(r'[^A-Za-z0-9]', '', reverse)
+        
+        s = s.lower()
+        s = re.sub(r'[^A-Za-z0-9]', '', s)
+
+        print(s)
+        print(reverse)
+
+        return reverse == s
